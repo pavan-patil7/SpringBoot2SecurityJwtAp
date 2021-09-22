1b6f034443c4259386ed9dddf7d62b1b86f24991
pavan-patil7 <59682062+pavan-patil7@users.noreply.github.com>
2021-09-22 22:27:18 +0530

    Update User.java

--- src/main/java/in/nit/raghu/entity/User.java
+++ src/main/java/in/nit/raghu/entity/User.java
@@ -6,7 +6,7 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence..Table;
 
 import lombok.Data;
 
